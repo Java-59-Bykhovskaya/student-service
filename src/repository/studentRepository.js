1432a5027a6492e60bb4216e36729f59ae0b50cb
GulnaraBykhovskaya <bykhovskaya.buro@gmail.com>
2025-05-26 14:01:04 +0300

    'server with MongoDB'

--- src/repository/studentRepository.js
+++ src/repository/studentRepository.js
@@ -1,29 +1,10 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-
-const dbName = 'java59';
-const client = new MongoClient(process.env.MONGO_URI);
 let collection;
 
-export async function connect () {
-  // if ((!client.topology && client.topology.isConnected())) {
-  //   await client.connect();
-  // }
-
-  if (!client.topology?.isConnected()) {
-    await client.connect();
-  }
-
-  const db = client.db(dbName);
+export function init (db) {
   collection = db.collection('college');
 }
 
-
 export const addStudent = async ({ id, name, password }) => {
-  await connect();
   const exists = await collection.findOne({ _id: { id } });
   if (exists) {
     return false;
@@ -34,23 +15,19 @@ export const addStudent = async ({ id, name, password }) => {
 
 
 export const findStudent = async (id) => {
-  await connect();
   return await collection.findOne({ _id: id });
 };
 
 export const deleteStudent = async (id) => {
-  await connect();
   return await collection.findOneAndDelete({ _id: id });
 
 };
 
 export const updateStudent = async (id, data) => {
-  await connect();
   return collection.findOneAndUpdate({ _id: id }, { $set: data }, { returnDocument: 'after' });
 };
 
 export const addScore = async (id, { examName, score }) => {
-  await connect();
   const student = await collection.findOneAndUpdate({ _id: id }, { $set: { [`scores.${examName}`]: score } }, { returnDocument: 'after' });
   if (student) {
     student.scores[examName] = score;
@@ -59,7 +36,6 @@ export const addScore = async (id, { examName, score }) => {
 };
 
 export const findStudentByName = async (name) => {
-  await connect();
   return await collection.find({
     name: {
       $regex: `${name}`,
--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import studentRoutes from './routes/studentRoutes.js';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+import { init } from './repository/studentRepository.js';
 
-
+dotenv.config();
 const app = express();
 const port = 8080;
+const dbName = 'java59';
+const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(studentRoutes);
@@ -11,6 +16,18 @@ app.use((req, res) => {
   res.status(404).type('text/plain').send('Not Found');
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}. Press Cntrl + C to finish.`);
-});
+async function startServer () {
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    init(db);
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}. Press Cntrl + C to finish.`);
+    });
+  } catch (err) {
+    console.log('Failed to connect MondoDB ', err);
+  }
+}
+
+startServer();
+
